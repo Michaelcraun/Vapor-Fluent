7eb2c9807857b12fc86d7319810472cd4b82a7aa
Michael Craun <michael.craun@gmail.com>
2020-12-12 16:43:21 -0500

    Completed Lesson 7-34 -- Get All Movies and Get Movie by Id: + Added routes to get all movies and to get a specific movie by ID

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,6 +3,17 @@ import Vapor
 
 func routes(_ app: Application) throws {
     
+    app.get("movies") { req in
+        Movie.query(on: req.db).all()
+    }
+    
+    app.get("movies", ":id") { req -> EventLoopFuture<Movie> in
+        
+        Movie.find(req.parameters.get("id"), on: req.db)
+            .unwrap(or: Abort(.notFound))
+        
+    }
+    
     // returns a "Promise", like in JavaScript
     app.post("movies") { (req) -> EventLoopFuture<Movie> in
         
